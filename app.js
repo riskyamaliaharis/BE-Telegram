655d4ee6f1e66dea4574fbb917c24ff69740ab95
Bagusth15 <bagustri15@gmail.com>
2021-01-20 13:02:51 +0700

    add config socket.io

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const routerNavigation = require("./src/routesNavigation");
+// ==============================
+const socket = require("socket.io");
+// ==============================
 
 const app = express();
 app.use(cors());
+
+// ==============================
+const http = require("http");
+const server = http.createServer(app);
+const io = socket(server);
+io.on("connection", (socket) => {
+  console.log("Socket.io Connect !");
+});
+// ==============================
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,6 +25,6 @@ app.use(morgan("dev"));
 
 app.use("/", routerNavigation);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
